36b9151faa60676428d14bf77278f18324a884c7
Tracy Chung <chuiyingchungccy@gmail.com>
2025-04-29 15:24:11 -0700

    Added about page

---
+++ client/app/about/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+const page = () => {
+    return (
+        <div>
+            Team Name: DTC-07
+            Team Members:
+            <ul>
+                <li> Emanuel Molla </li>
+                <li> Woojin Song </li>
+                <li> Jimmy Cho </li>
+                <li> Tracy Chung </li>
+                <li> Tushit Garg </li>
+            </ul>
+        </div>
+    )
+}
+
+export default page
