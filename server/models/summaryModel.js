c4bd4a3d5c34a1f834f76aa52e11b6ab92c68364
Tracy Chung <chuiyingchungccy@gmail.com>
2025-04-30 14:10:05 -0700

    Add summary model

---
+++ server/models/summaryModel.js
@@ -0,0 +1,17 @@
+// import mongoose from 'mongoose';
+const mongoose = require("mongoose");
+
+const summarySchema = new mongoose.Schema({
+    title: { 
+        type: String, 
+        required: true },
+    content: { 
+        type: String, 
+        required: true },
+    subject: { String,
+        required: true },
+}, { _id: true, timestamps: true });
+
+const summaryModel = mongoose.model('summary', summarySchema);
+// export default summaryModel
+module.exports = summaryModel;
