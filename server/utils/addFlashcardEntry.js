78e04a72cbfc6e530212282f9de0e661c5499227
Woojin Song <wsong31@my.bcit.ca>
2025-05-01 21:23:05 -0700

    Add addFlashcardEntry function and fix flashcard schema

---
+++ server/utils/addFlashcardEntry.js
@@ -0,0 +1,11 @@
+const flashcardSetModel = require("../models/flashcardModel")
+
+const addFlashcardEntry = async (flashcard) => {
+  // Create and save new flashcard
+  const newFlashcard = await flashcardSetModel.create(flashcard);
+
+  // Return the id of the flashcard
+  return newFlashcard._id;
+};
+
+module.exports = addFlashcardEntry;
