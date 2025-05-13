b6f8dc1f5f3a095b3279b8274b0547a54a192db5
Woojin Song <wsong31@my.bcit.ca>
2025-05-13 12:19:15 -0700

    Edit community template

--- client/app/test2/page.jsx
+++ client/app/test2/page.jsx
@@ -1,41 +1,37 @@
 import CommunityCard from '@/components/Community/CommunityCard'
 import React from 'react'
 import { Button } from '@/components/ui/button';
-import { Plus } from 'lucide-react';
 
 const page = () => {
-
+  const num_cards = 15;
   return (
     <>
       <div className="flex flex-col w-full justify-center items-center p-6">
         {/* Header */}
         <div className="container">
           <div className="flex justify-between w-full items-center mb-6">
-            <h1 className="text-2xl font-bold">Dashboard</h1>
+            <h1 className="text-2xl font-bold">Community</h1>
             <Button style={{ cursor: 'pointer' }}>
-              <Plus className="mr-2 h-4 w-4" />
-              Create
+              Sort
             </Button>
           </div>
 
           {/* Resource cards */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
-              <div className="border rounded-lg hover:shadow-md transition-shadow overflow-hidden">
+            {Array.from({ length: num_cards }).map((_, i) => (
+              <div
+                key={i}
+                className="border rounded-lg hover:shadow-md transition-shadow overflow-hidden"
+              >
                 <CommunityCard />
               </div>
+            ))}
           </div>
         </div>
       </div>
     </>
 
   );
-
-  return (
-    <div>
-
-      <CommunityCard />
-    </div>
-  )
 }
 
 export default page
