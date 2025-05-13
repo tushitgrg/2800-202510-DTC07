e8cb86103f7e0faddab50b920ce58adbaf4423ec
Woojin Song <wsong31@my.bcit.ca>
2025-05-12 19:56:19 -0700

    Add template community card and test its styling

---
+++ client/app/test2/page.jsx
@@ -0,0 +1,13 @@
+import CommunityCard from '@/components/Community/CommunityCard'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+
+     <CommunityCard/>
+    </div>
+  )
+}
+
+export default page
