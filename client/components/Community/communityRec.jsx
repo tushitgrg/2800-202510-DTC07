1fffd9c08ad11c02dbf39fe7efc65fdd8973dda9
Tracy Chung <chuiyingchungccy@gmail.com>
2025-05-15 13:34:48 -0700

    Add the logic for asking user to fill in school if empty

--- client/components/Community/communityRec.jsx
+++ client/components/Community/communityRec.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import CommunityCard from "@/components/Community/CommunityCard";
+import { useRouter } from "next/navigation";
 
-const CommunityRecomm = ({ userSchool = "BCIT", resources = [] }) => {
+
+const CommunityRecomm = ({ userSchool, resources}) => {
+    const router = useRouter();
+
+    if (!userSchool) {
+        return (
+            <div className="border border-dashed border-gray-600 rounded-2xl p-10 mt-4 bg-gradient-to-b from-gray-900 to-black flex flex-col items-center space-y-4">
+                <h2 className="text-md text-white">
+                    You haven't selected your school yet.
+                </h2>
+                <button
+                    onClick={() => router.push("/profile")}
+                    className="bg-gray-600 text-white px-4 py-2 rounded-full hover:bg-gray-700 transition"
+                >
+                    Go to Profile to Set School
+                </button>
+            </div>
+        );
+    }    
+
     const schoolResources = resources.filter(
-        (resource) => resource.school === userSchool
+        (resource) => resource.school?.toLowerCase().trim() === userSchool.toLowerCase().trim()
     );
 
     return (
