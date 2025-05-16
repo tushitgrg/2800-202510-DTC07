fdc4dbfc4e5f255322c1f2f0ad22cba674646d56
Tracy Chung <chuiyingchungccy@gmail.com>
2025-05-15 18:20:28 -0700

    Remove unused code from CommunityRec

--- client/components/Community/communityRec.jsx
+++ client/components/Community/communityRec.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import CommunityCard from "@/components/Community/CommunityCard";
 import { useRouter } from "next/navigation";
 
 
-const CommunityRecomm = ({ userSchool, resources}) => {
+const CommunityRecomm = ({ userSchool }) => {
     const router = useRouter();
     const normalizedUserSchool = userSchool?.trim();
 
@@ -22,38 +21,9 @@ const CommunityRecomm = ({ userSchool, resources}) => {
             </div>
         );
     }    
-
-    const normalize = (str) => (str ? str.trim().toLowerCase() : "");
-    const schoolResources = resources.filter(
-        (resource) => normalize(resource.school) === normalize(normalizedUserSchool)
-    );
     
-
-    return (
-        <div className="border border-dashed border-gray-600 rounded-2xl p-10 mt-4 bg-gradient-to-b from-gray-900 to-black">
-            <h2 className="text-md text-white mb-4">
-                Here are some recommended resources from your school "{normalizedUserSchool}" you might find helpful.
-            </h2>
-
-            {schoolResources.length > 0 ? (
-                <div className="grid grid-cols-2 md:grid-cols-3 gap-10">
-                    {schoolResources.map((resource, index) => (
-                        <CommunityCard
-                            key={index}
-                            title={resource.title}
-                            school={resource.school}
-                            date={resource.date}
-                            tags={resource.tags}
-                        />
-                    ))}
-                </div>
-            ) : (
-                <div className="flex justify-center items-center h-32 border rounded-lg text-gray-400">
-                    N/A
-                </div>
-            )}
-        </div>
-    );
+    return null;
+       
 };
 
 export default CommunityRecomm;
