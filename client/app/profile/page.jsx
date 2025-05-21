f97597ce71449816592ba0b301f632344473f16e
Tracy Chung <chuiyingchungccy@gmail.com>
2025-05-20 19:47:11 -0700

    add jsdoc to profile component

--- client/app/profile/page.jsx
+++ client/app/profile/page.jsx
@@ -5,6 +5,17 @@ import ProfileCard from "@/components/profile/profile";
 import Loading from "@/components/Loading";
 import { ServerUrl } from "@/lib/urls";
 
+/**
+ * Renders the user's profile page.
+ * 
+ * This component fetches the authenticated user's data using the Google OAuth cookie,
+ * then displays a profile card using the <ProfileCard /> component.
+ * If the user is not authenticated, it redirects to the Google login page.
+ * A loading spinner is shown while user data is being fetched.
+ *
+ * @returns {JSX.Element} The profilePage component.
+ */
+
 export default function profilePage() {
   const [googleUser, setGoogleUser] = useState(null);
 
