a4bb3a501d33f76ddd409434d9032900084f9930
Tracy Chung <chuiyingchungccy@gmail.com>
2025-05-20 19:43:30 -0700

    Add jsdoc to AboutUs component

--- client/app/about/page.jsx
+++ client/app/about/page.jsx
@@ -1,5 +1,14 @@
 import AboutUs from "@/components/aboutUs/aboutUs";
 
+/**
+ * Renders the About Page component which displays information about the project team and purpose.
+ * This page imports and displays the <AboutUs /> component.
+ *
+ * It serves a static informational role and does not include interactivity or backend integration.
+ *
+ * @returns {JSX.Element} The AboutPage component.
+ */
+
 export default function AboutPage() {
   return (
     <main className="flex-1 mt-20">
