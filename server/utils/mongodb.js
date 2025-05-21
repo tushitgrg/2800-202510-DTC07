4a50e5a8e80ee63b6a1b25b162e669b4c70d09b8
Emanuel-DevX <jefymoxi@gmail.com>
2025-05-21 01:39:21 -0700

    Document connectToDB for single connect

--- server/utils/mongodb.js
+++ server/utils/mongodb.js
@@ -2,6 +2,14 @@ const mongoose = require("mongoose");
 
 let isConnected = false;
 
+/**
+ * Establishes a singleton connection to the MongoDB database.
+ * Prevents redundant reconnections if already connected.
+ *
+ * @async
+ * @function connectToDB
+ * @returns {Promise<void>}
+ */
 async function connectToDB() {
   if (isConnected) return;
 
