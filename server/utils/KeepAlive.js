813368827c2e5c1014e73520d06e50059bb18513
Tushit <tushitgarg123@gmail.com>
2025-05-22 22:35:13 -0700

    Add a self-hit endpoint

---
+++ server/utils/KeepAlive.js
@@ -0,0 +1,15 @@
+//this file sends requests every interval to keep the render instance always alive.
+const interval = 300000; // Interval in milliseconds (30 seconds)
+
+//Reloader Function
+function reloadWebsite() {
+  axios.get('https://server.scholiast.webios.link/keep-alive')
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
