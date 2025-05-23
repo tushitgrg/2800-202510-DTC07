ec2850b5c7f6c26be4c23778cde4c0b924172118
Woojin Song <wsong31@my.bcit.ca>
2025-05-22 21:47:26 -0700

    Add documentation to Footer.jsx

--- client/components/layout/Footer.jsx
+++ client/components/layout/Footer.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import Link from "next/link";
 
+/**
+ * Footer component
+ * Renders the site footer with copyright information.
+ *
+ * @component
+ * @returns {JSX.Element} Footer element
+ */
 const Footer = () => {
   return (
-    <footer className=" w-screen ">
+    <footer className="w-screen bg-gray-100 dark:bg-gray-900">
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
         <div className="sm:flex sm:items-center sm:justify-center">
-      
-
-          <p className="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right dark:text-gray-400">
-            Copyright &copy; 2025. All rights reserved.
+          {/* You can add logo or navigation links here if needed */}
+          <p className="mt-4 text-center text-sm text-gray-500 dark:text-gray-400 lg:mt-0 lg:text-right">
+            {/* Display current year dynamically */}
+            &copy; {new Date().getFullYear()}. All rights reserved.
           </p>
         </div>
       </div>
